8ec178e37d964a05ff70008111277ae875999e61
Sampel65 <sampeldan65@gmail.com>
2022-10-16 02:51:46 +0100

    first commit

---
+++ ApiTest/ApiTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  ApiTestApp.swift
+//  ApiTest
+//
+//  Created by Sampel on 07/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct ApiTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
